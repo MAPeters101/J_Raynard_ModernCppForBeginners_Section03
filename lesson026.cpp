842428988f4916ba63faed3971e1b56fa4d6116e
Mark Peters <mapeters101@earthlink.net>
2024-11-26 13:50:56 -0500

    Work on lesson026.

---
+++ lesson026.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	int input;
+	std::cout << "Please enter a number\n";
+	std::cin >> input;
+	std::cout << "You entered the number " << input << "\n\n\n";
+
+	std::string input_string;
+	std::cout << "Please enter a word\n";
+	std::cin >> input_string;
+	std::cout << "You entered the word " << input_string << "\n\n";
+	
+	
+	
+	
+	return 0;
+}

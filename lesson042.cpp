5b794dbbf5a24cc1e20dd4fc744b7451d80ce401
Mark Peters <mapeters101@earthlink.net>
2024-11-26 18:46:48 -0500

    Work on lesson042.

---
+++ lesson042.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	string input{""};
+	cout << "Enter some lines of text.  Enter \"done\" when you want to stop\n";
+	while (input != "done")
+		cin >> input;
+	cout << "Thank you.  Goodbye!\n";
+
+
+	int i{ 0 };
+	cout << "Enter an integer value\n";
+	while (cin >> i)
+		cout << "You entered " << i << "\n";
+	cout << "An error or end of file occurred\n";
+
+	string s{ "" };
+	int word_count{ 0 };
+	cout << "Enter some words\n";
+	while (cin >> s)
+		++word_count;
+	cout << "You entered " << word_count << " words\n";
+
+	return 0;
+}
+*/

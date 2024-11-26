da1d776416857eb38a5f55c0362cf7f41b3e8bb1
Mark Peters <mapeters101@earthlink.net>
2024-11-26 18:11:28 -0500

    Work on lesson040.

---
+++ lesson040.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	char c{ ' ' };
+	cout << "Type a single character followed by return" << endl;
+	cin >> c;
+	cout << "You typed " << c << "\n";
+
+	int x{ c };
+	cout << "The ASCII code for " << c << " is " << x << "\n";
+
+	char a{ 'c' }, b{ 'd' };
+	char e{ a + b };
+	cout << "a * b = " << a * b << "\n";
+	cout << "e = " << e << "\n";
+
+	return 0;
+}
+*/
+

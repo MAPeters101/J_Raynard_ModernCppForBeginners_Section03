fbdb7888bdf38f7c04b5899494e296f9beec13c0
Mark Peters <mapeters101@earthlink.net>
2024-11-26 13:39:53 -0500

    Work on lesson025.

---
+++ lesson025.cpp
@@ -0,0 +1,17 @@
+// Examples of type representations
+#include <iostream>
+/*
+int main() {
+	int x;
+	x = 2;
+	std::cout << "The answer is " << 42 << "\n";
+	std::cout << "The answer is " << 0x2A << "\n";
+	std::cout << "The answer is " << 052 << "\n";
+	std::cout << "The answer is " << 0b101010 << "\n";
+	std::cout << "\nThe year is " << 2024 << "\n";
+
+
+	return 0;
+}
+*/
+

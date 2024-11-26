213298df27d960d99d66d8355bfefb3b3ca06330
Mark Peters <mapeters101@earthlink.net>
2024-11-26 17:02:30 -0500

    Work on lesson037.

---
+++ lesson037.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*
+int main() {
+	int x{ 4 }, y{ 3 };
+	cout << "x is greater than y: " << (x > y) << "\n";
+
+	bool is_greater{ x > y };
+	cout << "x is greater than y: " << boolalpha << is_greater << noboolalpha << "\n";
+
+	bool is_less{ x < y };
+	cout << "x is less than y: " << boolalpha << is_less << noboolalpha << "\n";
+
+	cout << "==========\n\n";
+
+
+	int a{ 2 }, b{ 2 };
+
+	bool is_less_equal{ a <= b };
+	cout << "a is less than or equal to b: " << boolalpha << is_less_equal << noboolalpha << "\n";
+
+	return 0;
+}
+*/

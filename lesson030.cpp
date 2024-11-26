3a3e92e1d0d292f37e7992a75d2a948b9c18125b
Mark Peters <mapeters101@earthlink.net>
2024-11-26 15:35:10 -0500

    Work on lesson030.

---
+++ lesson030.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+/*
+int main() {
+	int a{ 1 };
+	int b{ 2 };
+	std::cout << "Before assignment: a = " << a << ", b = " << b << "\n";
+	a = b;
+	std::cout << "After assignment: a = " << a << ", b = " << b << "\n\n";
+
+	std::string str1{ "Hello" };
+	std::string str2{ "Goodbye" };
+	std::cout << "Before assignment: str1 = " << str1 << ", str2 = " << str2 << "\n";
+	str1 = str2;
+	std::cout << "After assignment: str1 = " << str1 << ", str2 = " << str2 << "\n";
+
+
+
+	return 0;
+}
+*/

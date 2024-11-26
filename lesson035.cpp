8a95b274766bc95ed23898c5c0d255c95d4f340e
Mark Peters <mapeters101@earthlink.net>
2024-11-26 16:39:46 -0500

    Work on lesson035.

---
+++ lesson035.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+/*
+int main() {
+	cout << "7 + 3 = " << 7 + 3 << endl;
+	cout << "7 - 3 = " << 7 - 3 << endl;
+	cout << "7 * 3 = " << 7 * 3 << endl;
+	cout << "7 / 3 = " << 7 / 3 << endl;
+	cout << "7 % 3 = " << 7 % 3 << endl;
+
+	cout << "==========\n\n";
+
+	int x{ 7 }, y{ 3 }, z{ 0 };
+
+	z = x + y;
+	cout << "x + y = " << z << endl;
+
+	z = x - y;
+	cout << "x - y = " << z << endl;
+
+	z = x * y;
+	cout << "x * y = " << z << endl;
+
+	z = x / y;
+	cout << "x / y = " << z << endl;
+
+	z = x % y;
+	cout << "x % y = " << z << endl;
+
+
+
+	return 0;
+}
+*/

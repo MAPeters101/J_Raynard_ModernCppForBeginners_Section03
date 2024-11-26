b8aa87ef034c6bb73909d5007cb0a9fe01a10502
Mark Peters <mapeters101@earthlink.net>
2024-11-26 17:33:38 -0500

    Work on lesson038.

---
+++ lesson038.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int a{ 2 }, b{ 2 };
+	if (a == b)
+		cout << "a and b have the same value.\n";
+	cout << "This is the next line after the if statement\n\n";
+
+
+	int answer{ 0 };
+	cin >> answer;
+	if (answer == 42)
+		cout << "That's correct!\n";
+
+	return 0;
+}
+*/

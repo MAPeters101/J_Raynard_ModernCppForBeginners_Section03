af86523361a87242876c23bb89afec5e530249d0
Mark Peters <mapeters101@earthlink.net>
2024-11-26 16:30:58 -0500

    Work on lesson034.

---
+++ lesson034.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::hex;
+
+/*
+int main() {
+
+	cout << "The answer is " << hex << 42 << "\n";
+	cout << "I am " << 42 << " years old" << "\n";
+	cout << "I am " << std::dec << 42 << " years old" << "\n";
+
+
+
+	return 0;
+}
+*/
+

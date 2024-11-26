b1ea9f45253104413e3a15644642900fa965742f
Mark Peters <mapeters101@earthlink.net>
2024-11-26 18:29:32 -0500

    Work on lesson041.

---
+++ lesson041.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	double d{ 0 };
+	cin >> d;
+	cout << d << "\n";
+
+	double pi{ 3.14159 };
+	double avogadro{ 6E23 };
+	double diff{ avogadro - pi };
+	cout << "diff == avogadro: " << (diff == avogadro) << "\n";
+
+	cout << "==========\n\n";
+
+	double d1{ 3.8 };
+	double d2 = d1 + 1;
+	if (d2 < 5)
+		cout << "d2 is less than 5\n\n";
+
+
+	int i1{ 2 };
+	double d3 = i1;
+	int i2 = d3;
+	if (i1 == i2)
+		cout << "This is a safe type conversion\n\n";
+
+	double d4{ 1.76 };
+	int i3 = d4;
+	double d5 = i3;
+	if (d4 != d5)
+		cout << "This is an unsafe type conversion\n\n";
+
+	return 0;
+}
+*/
+
+

c1393cbadf4debda88dd8f8ce4fe44f2b518ce0c
Mark Peters <mapeters101@earthlink.net>
2024-11-26 15:52:51 -0500

    Work on lesson031.

---
+++ lesson031.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/*
+int main() {
+	auto x{ 5 };
+	auto addr{ &x };
+	std::cout << "Value of x is " << x << "\n";
+	std::cout << "Address of x is " << addr << "\n";
+	std::cout << "Value stored in addr is " << *addr << "\n";
+
+	return 0;
+}
+*/
+

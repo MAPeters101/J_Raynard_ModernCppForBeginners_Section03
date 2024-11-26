22b80d76f90a1a2f7e1e21aa5e16568f4fd0ad8e
Mark Peters <mapeters101@earthlink.net>
2024-11-26 14:31:19 -0500

    Work on lesson028.

---
+++ lesson028.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+/*
+int main() {
+	int input;
+	std::cout << "Please enter a number: ";
+	std::cin >> input;
+	std::cout << "You entered the number " << input << "\n";
+
+	std::string string_input;
+	std::cout << "Please enter a word: ";
+	std::cin >> string_input;
+	std::cout << "You entered the word " << string_input << "\n";
+
+	std::string first_name, surname;
+	std::cout << "Please enter your first name and surname: ";
+	std::cin >> first_name >> surname;
+	std::cout << "Hello, " << first_name << " " << surname << "!\n";
+	std::cout << "==========\n\n";
+
+	std::string name;
+	int age;
+	std::cout << "Please enter your name: ";
+	std::cin >> name;
+	std::cout << "Hello, " << name << ", how old are you? ";
+	std::cin >> age;
+	std::cout << "You are " << age << " yearas old.\n\n";
+
+
+
+	return 0;
+}
+
+*/
+

8d96e43aadc1ba32bca6f9ab09531cf4cfa7461b
Mark Peters <mapeters101@earthlink.net>
2024-11-26 16:47:03 -0500

    Work on lesson036.

---
+++ lesson036.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+/*
+int main() {
+	int x;
+	std::cin >> x;
+	std::cout << "x is " << x << "\n";
+	x = x + 5;
+	std::cout << "After adding 5 to x, it now has the value " << x << "\n";
+
+	return 0;
+}
+*/
+

51367768cac04519b62108e9244949f921c22a39
Mark Peters <mapeters101@earthlink.net>
2024-11-26 17:43:12 -0500

    Work on lesson039.

---
+++ lesson039.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	string s1{ "one " };
+	string s2{ "two" };
+	string s3{ "" };
+	s3 = s1 + s2;
+	cout << s1 << " + " << s2 << " = " << s3 << "\n\n\n";
+
+
+	s1 = "abc";
+	s2 = "abcd";
+	s3 = "abd";
+	string s4{ "ABC" };
+
+	cout << "s1 == s2: " << (s1 == s2) << "\n";
+	cout << "s1 == s3: " << (s1 == s3) << "\n";
+	cout << "s1 == s4: " << (s1 == s4) << "\n\n";
+
+	cout << "s1 != s2: " << (s1 != s2) << "\n";
+	cout << "s1 != s3: " << (s1 != s3) << "\n";
+	cout << "s1 != s4: " << (s1 != s4) << "\n\n";
+
+
+	return 0;
+}
+*/

6e91530c45e2fbdc8ffae14cbdfc22878e02051a
Mark Peters <mapeters101@earthlink.net>
2024-11-27 13:40:53 -0500

    Work on lesson044.

---
+++ lesson044.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	const int i{ 3 };
+	int x{ 0 };
+	cin >> x;
+	const int y{ x };
+	cout << y << endl;
+
+	constexpr int a{ 3 };
+	// a = 4;
+	cout << a + 1;
+	constexpr int b{ a + 1 };
+
+	return 0;
+}
+
+

fccee33ce1c35865ebc681dbf648e55a4379b383
Mark Peters <mapeters101@earthlink.net>
2024-11-27 14:11:21 -0500

    Work on lesson047.

---
+++ lesson047.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n{ 0 };
+	cin >> n;
+
+	if (n == 1)
+		cout << "One-eyed monster\n";
+	else {
+		if (n == 2)
+			cout << "Two-eyed monster\n";
+		else
+			cout << "Many-eyed monster\n";
+	}
+
+	if (n == 1)
+		cout << "One-eyed monster\n";
+	else if (n == 2)
+		cout << "Two-eyed monster\n";
+	else
+		cout << "Many-eyed monster\n";
+	
+
+
+	return 0;
+}
+

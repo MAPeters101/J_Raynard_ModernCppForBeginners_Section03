4d8dd2acbe010a4a4214f6edfc821d075ceeeac0
Mark Peters <mapeters101@earthlink.net>
2024-11-27 14:03:24 -0500

    Work on lesson046.

---
+++ lesson046.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main() {
+	int answer{ 0 };
+	cin >> answer;
+	//if (answer == 42) {
+	//	cout << "That's correct! You have won the game!\n";
+	//	cout << "Not many people would have guessed that!\n\n";
+	//}
+	//else {
+	//	cout << "Sorry, " << answer << " is not the right answer.  Try again!\n";
+	//	int new_guess{ 0 };
+	//	cin >> new_guess;
+	//}
+	//
+	// cout << new_guess;
+
+
+	while (answer != 42) {
+		cout << "Sorry, " << answer << " is not the right answer\n";
+		cout << "Please try again: " << flush;
+		cin >> answer;
+	}
+
+	cout << "That's correct! You have won the game!\n";
+	cout << "Not many people would have guessed that!\n\n";
+
+
+	return 0;
+}
+*/
